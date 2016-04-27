ad8b8280ce1e8aacd4a923d9b00bf6550d74fba8
Mirosław Szul <szulem@o2.pl>
2016-04-27 11:23:52 +0200

    Recipe blog

---
+++ app/models/recipe.rb
@@ -0,0 +1,19 @@
+class Recipe < ActiveRecord::Base
+	belongs_to :user
+
+	has_many :ingredients
+	has_many :directions
+
+	accepts_nested_attributes_for :ingredients, 
+																reject_if: proc { |attributes| attributes['name'].blank? },
+																allow_destroy: true
+
+	accepts_nested_attributes_for :directions,
+																reject_if: proc { |attributes| attributes['step'].blank? },
+																allow_destroy: true
+
+	validates :title, :description, :image, presence: true
+
+  has_attached_file :image, styles: { medium: "400x400#", thumb: "100x100>" }, default_url: ":style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
